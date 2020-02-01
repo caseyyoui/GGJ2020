5225d4b5a1785f3c60c06268e0c66af2a8868820
Anne Gingras <anne.gingras@youi.tv>
2020-02-01 11:44:21 -0500

    character animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     public float m_MovementSpeed = 1.0f;
     private Vector2 m_dir;
+    public SpriteRenderer spriteRenderer;
 
+    public Sprite spriteUp;
+    public Sprite spriteDown;
+    public Sprite spriteLeft;
+    public Sprite spriteRight;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +25,24 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 vel = m_dir * (m_MovementSpeed * Time.deltaTime);
         transform.position = transform.position + vel;
+        
+        // handle animations
+        if (m_dir.x < -.05f)
+        {
+            spriteRenderer.sprite = spriteLeft;
+        }
+        else if (m_dir.x > .05f)
+        {
+            spriteRenderer.sprite = spriteRight;
+        }
+        if (m_dir.y < -.05f)
+        {
+            spriteRenderer.sprite = spriteDown;
+        }
+        else if (m_dir.y > .05f)
+        {
+            spriteRenderer.sprite = spriteUp;
+        }
     }
 
 	public void Move(InputAction.CallbackContext context)
